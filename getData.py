0bbd267d01b354d4ddc9cb4da5b505b14ad589f5
Timothy Choh <tchoh73@wheatley.acm.cs>
2017-04-29 17:38:23 -0500

    updated python script and README will finish

--- getData.py
+++ getData.py
@@ -6,7 +6,7 @@ from dateutil.parser import parse
 from datetime import datetime, timedelta
 
 
-formatstring = '{"courseName": "%s", "courseNum": %d, "CRN": %d, "classTime": " %s", "finalStart": new Date(%d,%d,%d,%d,0,0,0), "finalEnd": new Date(%d,%d,%d,%d,0,0,0), "finalLocation": "%s", "finalRoom": "%s"},'
+formatstring = '{{"courseName": "{}", "courseNum": {}, "CRN": {}, "classTime": " {}", "finalStart": new Date({},{},{},{},{},0,0), "finalEnd": new Date({},{},{},{},{},0,0), "finalLocation": "{}", "finalRoom": "{}"}},'
 
 with open(sys.argv[1], 'r') as infile:
     reader = csv.reader(infile, delimiter=',', quotechar='"')
@@ -14,25 +14,36 @@ with open(sys.argv[1], 'r') as infile:
     next(iterreader)
     for row in iterreader:
         col1 = row[0].split(" ")
-        courseName = col1[0]
-        courseNum = col1[1]
-        crn = col1[3]
-        col1 = row[0].split(" - ")
-        classTime = ""
-        if (len(col1) > 2):
-            classTime = col1[2]
-        building = row[3]
-        room = row[4]
-        day = row[1]
-        finaltime = row[2].split(" - ")
-        starttime = finaltime[0]
-        endtime = finaltime[1]
-        dayp = parse(day)
-        endtimep = parse(endtime)
-        enddatetimep = dayp
-        enddatetimep = enddatetimep.replace(hour=endtimep.hour, minute=endtimep.minute)
-        startdatetimep = dayp
-        startdatetimep = startdatetimep.replace(hour=starttimep.hour, minute=starttimep.minute)
-        print(startdatetimep)
-        print(enddatetimep)
-        print(", ".join((courseName, courseNum, crn, classTime, building, room, starttime, endtime)))
+        if (col1[0] == 'CS'):
+            courseName = col1[0]
+            courseNum = col1[1]
+            crn = col1[3]
+            col1 = row[0].split(" - ")
+            classTime = ""
+            if (len(col1) > 2):
+                classTime = col1[2]
+            building = row[3]
+            room = row[4]
+            day = row[1].split(" ")
+            finaltime = row[2].split(" - ")
+            finalend = finaltime[1].split(" ")
+            starthour = 0
+            startmin = 0
+            endhour = 0
+            endmin = 0
+            start = finaltime[0].split(":")
+            end = finaltime[1].split(":")
+            if (finalend[1] == "pm" or finalend[1] == "PM"):
+                if (int(end[0]) > 2 and int(end[0]) != 12):
+                    start[0] = int(start[0]) + 12
+                    end[0] = int(end[0]) + 12
+                elif (int(end[0]) == 1 or int(end[0]) == 2):
+                    end[0] = int(end[0]) + 12
+            starthour = start[0]
+            startmin = start[1]
+            endhour = end[0]
+            endmintemp = end[1].split(" ")
+            endmin = endmintemp[0]
+
+            print(formatstring.format(courseName, courseNum, crn, classTime, 2017, 4, day[2], starthour, startmin, 2017, 4, day[2], endhour, endmin, building, room))
+            #print(", ".join((courseName, courseNum, crn, classTime, building, room, str(starthour), str(startmin), str(endhour), str(endmin))))
