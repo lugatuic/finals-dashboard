6610c42cca5c965e572fecff58ad6105c60bab91
Chase 李 <clee231@uic.edu>
2018-05-08 19:37:34 -0500

    Adding reset timers to reset.

--- anglist.js
+++ anglist.js
@@ -170,3 +170,24 @@ app.filter('orderObjectBy', function(){
     return array;
  }
 });
+
+function refreshAt(hours, minutes, seconds) {
+    var now = new Date();
+    var then = new Date();
+
+    if(now.getHours() > hours ||
+       (now.getHours() == hours && now.getMinutes() > minutes) ||
+        now.getHours() == hours && now.getMinutes() == minutes && now.getSeconds() >= seconds) {
+        then.setDate(now.getDate() + 1);
+    }
+    then.setHours(hours);
+    then.setMinutes(minutes);
+    then.setSeconds(seconds);
+
+    var timeout = (then.getTime() - now.getTime());
+    setTimeout(function() { window.location.reload(true); }, timeout);
+}
+
+refreshAt(6, 0, 0);
+refreshAt(21, 0, 0);
+refreshAt(23,59,59);
